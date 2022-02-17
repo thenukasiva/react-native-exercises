79fbf154c5011e04ac0669b7dc53fd00393e82d7
Thenuka Siva <38299804+thenukasiva@users.noreply.github.com>
2022-02-17 18:51:48 -0500

    playing around with React components,  props and state

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState } from 'react';
+import { Text, View, TextInput, Image} from 'react-native';
 
-export default function App() {
+const getFullName = (firstName, lastName) => {
+  return firstName + " " + lastName;
+}
+
+// Child Component
+const Cat = (props) => {
+  const [isHappy, setIsHappy] = useState(true);
+  return ( 
+    <View>
+      <Text>Hello, I am {props.name}! I am feeling {isHappy ? "happy" : "sad"}</Text>
+      <Button 
+      onPress={() => {setIsHappy(false);}}
+      disabled={!isHappy}
+      title={isHappy ? "Make me happy again please!" : "Thank you!" }/>
+      <TextInput style={{
+        height: 25,
+        borderColor: 'gray',
+        borderWidth: 1
+      }}
+      defaultValue="What is your name? Type it here"
+      />
+    </View>
+    
+  );
+} 
+
+// Parent Component
+const Cafe = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello Thenuka!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Welcome!</Text>
+      <Cat name="Thenuka"/>
+      <Cat name="Siva"/>
+      <Cat name="Mana"/>
+
+      <Image 
+        source={{uri: "https://reactnative.dev/docs/assets/p_cat1.png"}}
+        style={{width: 100, height: 100}}/>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Cafe;
